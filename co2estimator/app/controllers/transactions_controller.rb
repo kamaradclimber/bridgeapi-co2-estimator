fe2d9276ae1a896656e5e17638a59e3c3dfb7e49
Grégoire Seux <kamaradclimber@gmail.com>
2022-04-17 16:12:51 +0200

    Allow basic edition of transactions
    Change-Id: Ia42392061f660c5f5554f18a3a00f44e79e3ef12

--- co2estimator/app/controllers/transactions_controller.rb
+++ co2estimator/app/controllers/transactions_controller.rb
@@ -8,4 +8,18 @@ class TransactionsController < ApplicationController
   def show
     @transaction = Transaction.find(params[:id])
   end
+
+  def update
+    transaction = Transaction.find(params[:id])
+    # FIXME: it is likely that we should filter:
+    # - who can update this transaction
+    permitted = params.require(:transaction).permit(:category_id, :description, :date)
+    if permitted.permitted?
+      transaction.update!(permitted)
+      redirect_to(transaction, notice: 'Updated!')
+    else
+      # FIXME: I'm not sure if this is ever called because the code raises when defining the permitted variable
+      redirect_to(transaction, alert: 'Missing parameters for the update')
+    end
+  end
 end
