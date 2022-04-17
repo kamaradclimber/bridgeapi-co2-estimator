d903c36d568f844263544e12a5c3e41e50a71072
Grégoire Seux <kamaradclimber@gmail.com>
2022-04-17 14:22:13 +0200

    Detect more transaction types
    Change-Id: Ia62f912e78d99b6fadf7d1f95c4842dd129c25f3

--- co2estimator/app/models/transaction.rb
+++ co2estimator/app/models/transaction.rb
@@ -39,6 +39,117 @@ class Transaction < ApplicationRecord
 
   def category_name
     BridgeApi::Dependencies.resolve(:categories)[category_id]['name']
+  rescue StandardError
+    # we are likely falling in a case where `category_id` has been set to nil instead of equality comparison
+    "unknown category #{category_id}"
+  end
+end
+
+class Electricity < Transaction
+  def self.match?(transaction)
+    transaction.category_id == 217
+  end
+
+  def co2_kg
+    # approximation: in france, electricity emits 50gCO2/kWh
+    # cost of kWh is 0.1740€/kWh (at least for EDF, my own provider is much cheaper)
+    # note: we don't count subscription cost
+    # note: this is an estimation from the money transfer. People interested in their CO2 emission
+    #       are likely to measure their own electricity consumption
+    emission_per_euro_in_kg = 50.0 / 1000 / 0.1740
+    amount.abs * emission_per_euro_in_kg
+  end
+
+  def icon
+    '💡'
+  end
+end
+
+class InternetAccess < Transaction
+  def self.match?(transaction)
+    transaction.category_id == 180
+  end
+
+  def co2_kg
+    # approximation: 3.95kgCO2/month according to ademe
+    # subscription for 1month costs 39.99€ (on free website at least)
+    # of course, we could simply associate one transaction to 3.95kg
+    # source: https://adsl.free.fr/co2.pl
+    emission_per_euro_in_kg = 3.95 / 39.99
+    amount.abs * emission_per_euro_in_kg
+  end
+
+  def icon
+    '🕸'
+  end
+end
+
+class FreeInternetAccess < InternetAccess
+  def self.match?(transaction)
+    super && transaction.description =~ /Free Telecom/
+  end
+
+  def co2_kg
+    # approximation: 1.7kgCO2/month according to free
+    # subscription for 1month costs 39.99€ (on free website)
+    # of course, we could simply associate one transaction to 1.7kg
+    # source: https://adsl.free.fr/co2.pl
+    emission_per_euro_in_kg = 1.7 / 39.99
+    amount.abs * emission_per_euro_in_kg
+  end
+
+  def icon
+    '🕸'
+  end
+end
+
+class Mobile < Transaction
+  def self.match?(transaction)
+    transaction.category_id == 277
+  end
+
+  def co2_kg
+    # approximation: 50gCO2/GB according to ademe
+    # subscription for 110GB costs 12€ (on free website)
+    # source: https://mobile.free.fr/account/conso-et-factures/empreinte-carbone + prices on free.fr
+    # source: https://expertises.ademe.fr/economie-circulaire/consommer-autrement/passer-a-laction/reconnaitre-produit-plus-respectueux-lenvironnement/dossier/laffichage-environnemental/affichage-environnemental-secteur-numerique
+    emission_per_euro_in_kg = 50.0 / 1000 * 110 / 12
+    amount.abs * emission_per_euro_in_kg
+  end
+
+  def icon
+    '📶'
+  end
+end
+
+class FreeMobile < Mobile
+  def self.match?(transaction)
+    super && transaction.description =~ /Free Mobile/
+  end
+
+  def co2_kg
+    # approximation: 24.3gCO2/GB according to free mobile website
+    # subscription for 110GB costs 12€ => 222.75gCO2/€
+    # source: https://mobile.free.fr/account/conso-et-factures/empreinte-carbone + prices on free.fr
+    emission_per_euro_in_kg = 24.3 / 1000 * 110 / 12
+    amount.abs * emission_per_euro_in_kg
+  end
+end
+
+class Withdrawals < Transaction
+  def self.match?(transaction)
+    transaction.category_id == 85
+  end
+
+  def co2_kg
+    # it's very hard to estimate what cash has been used to.
+    # Here we'll assume paying in cash is to pay small services or local products
+    # but that's a guess of course
+    0
+  end
+
+  def icon
+    '🤷️'
   end
 end
 
@@ -131,9 +242,25 @@ class Taxes < Transaction
   end
 end
 
+class Leetchi < Transaction
+  def self.match?(transaction)
+    transaction.category_id == 183 && transaction.description =~ /leetchi/i
+  end
+
+  def co2_kg
+    # approximation: We can't know how this money will be used. Let's ignore it for now
+    0
+  end
+
+  def icon
+    '🎁'
+  end
+end
+
 class AmazonDelivery < Transaction
   def self.match?(transaction)
-    transaction.category_id == 186 && transaction.description =~ /amzn/i
+    (transaction.category_id == 186 && transaction.description =~ /amzn/i) ||
+      (transaction.category_id == 184 && transaction.description =~ /amzn mktp/i)
   end
 
   def co2_kg
@@ -141,6 +268,7 @@ class AmazonDelivery < Transaction
     # in 2021, amazon emmitted 60.64B kg of CO2 (source: https://fortune.com/2021/06/30/amazon-carbon-footprint-pollution-grew/)
     # in 2020, its revenue was $386B, so 351B€
     # raw estimation is 0.1727 kgCO2/$
+    # of course we are just taking into account amazon value here (instead of the product fabrication)
     amount.abs * 0.1727
   end
 
